26b17efe9796b0473f187fa27d6d96cc9626ef38
Dep-Vik <deputatovaviktoria5@gmail.com>
2022-02-01 13:46:26 +0300

    ошибка 17 строка

--- includes/functions.php
+++ includes/functions.php
@@ -5,6 +5,13 @@ function get_url($page ='') {
 	return HOST . "/$page";
 }
 
+function get_page_title($title=''){
+if (!empty($title)) {
+	return SITE_NAME  . " - $title";
+	} else{
+		return SITE_NAME;
+}
+}
 function db() {
 	try {
 		return new PDO("mysql:host=". DB_HOST. ";dbname=". DB_NAME . ";charset=utf8", DB_USER, DB_PASS,
@@ -29,8 +36,12 @@ function db_query($sql, $exec = false) {
 
 function get_posts($user_id = 0) {
 	if ($user_id > 0) {
-		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id WHERE posts.user_id = $user_id;");
+		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id WHERE posts.user_id = $user_id;")->fetchAll();
 	} else {
-		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id;");
+		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id;") ->fetchAll();
 	}
-}
+	function get_user_info($login) {
+		return db_query("SELECT * FROM 'user' WHERE 'login' = '$login';")->fetchAll();
+	}
+} 
+// function add_user($login, $pass)
